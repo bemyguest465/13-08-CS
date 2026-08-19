02413094bdb18a4e2a23aab8e64ae4b6c17b98d5
bemyguest465 <bemyguest465@gmail.com>
2026-08-19 19:45:10 +0530

    Remove unused variable and calculation

--- bytestokbmbgb.c
+++ bytestokbmbgb.c
@@ -7,8 +7,5 @@ void main(){
     printf("The no. of KB is %f",bytes/(pow(2,10)));
     printf("\nThe no. of MB is %f",bytes/(pow(2,20)));
     printf("\nThe no. of GB is %f",bytes/(pow(2,30)));
-    int x;
-    x=(2^10);
-    printf("\nx=%d",x);
 }
 
