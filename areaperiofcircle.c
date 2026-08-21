90af9153463926eca315318c71b24e370f3af049
bemyguest465 <bemyguest465@gmail.com>
2026-08-21 21:11:55 +0530

    Delete areaperiofcircle.c

--- areaperiofcircle.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-    float rad;
-    printf("Enter the radius: ");
-    scanf("%f", rad);
-    printf("The circumference is %.2f", 2*(22/7)*rad);
-    printf("The area is %.2f", (22/7)*rad*rad);
-}
